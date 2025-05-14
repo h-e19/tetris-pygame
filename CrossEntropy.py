8541509f4143c60b8fc917bb476263d905daede1
arsh <143431419+akx16@users.noreply.github.com>
2025-05-14 22:33:09 +0500

    implemented cross entropy

--- CrossEntropy.py
+++ CrossEntropy.py
@@ -1,14 +1,16 @@
 import random
 from Tetris import *
 import numpy as np
+import statistics 
 
-POP_SIZE=10
+POP_SIZE=20
 numOfFactors = 6
 initial_mean = [0 for i in range(numOfFactors)]
 initial_variance= [100 for i in range(numOfFactors)]
+initial_std=np.sqrt(initial_variance)
+proportion = 0.25
+
 weights = [-1,1,-1,-1,-4,-1] #placeholder weights
-
-
 
 record_data = [] #mean, var, avg score of generations
 
@@ -19,48 +21,83 @@ score = main_menu_AI(win, weights)
 
 print('score: ', score)
 
-class game:
+class Game:
     def __init__(self, weights=[], score=0):
         self.weights=weights
         self.score = score
-        
-    def get_weights(self):
-        return self.weights
-    
-    def set_weights(self,w):
-        self.weights = w
-        
-    def get_score(self):
-        return self.score
-    
-    def set_score(self,s):
-        self.score=s
-        
     def play(self):
         #plays game and returns score 
         pass
 
 class CrossEntropy:
-    def __init__(self, popsize, mean=initial_mean, variance=initial_variance):
+    def __init__(self, popsize=POP_SIZE, mean=initial_mean, std=initial_std):
         self.popsize=popsize
         self.mean=mean
-        self.variance=variance
-        self.stds=np.sqrt(variance)
-        self.population = [game for i in range(popsize)]
+        self.std=std
+        self.population = [Game for i in range(popsize)]
     
     def generate_samples(self, samplesize):
-        weights_samples=np.random.normal(self.mean, self.stds, size=(samplesize, 6)) #generates n vectors of size 6 (n games)
+        weights_samples=np.random.normal(self.mean, self.std, size=(samplesize, 6)) #generates n vectors of size 6 (n games)
         return weights_samples
+    
+    def set_population(self):
+        weight_samples = self.generate_samples(20)
+        new_population=list[Game]
         
-    def evaluate(self, samples):
-        #returns fitness using game.play
-        pass
+        for weights in weight_samples:
+            new_population.append(Game(weights))
+            
+        self.population=new_population
+            
+        
+    def evaluate(self):
+        for game in self.population:
+            game.play()
         
     def selection(self):
         #selects best based on fitness
-        pass
+        num=proportion*self.popsize
+        sorted(self.population,key=lambda game: game.score, reverse=True)
+        selected=[]
+        for i in range(num):
+            selected.append(self.population[i])
+        return selected
 
-    def update_dist(self): 
+    def update_dist(self, selected:list[Game]): 
         #changes mean and std dev based on best samples
-        pass
+        newmean=[0 for i in range(numOfFactors)]
+        newstd=[0 for i in range(numOfFactors)]
         
+        newmean=[((w1+w2+w3+w4+w5)/5) for w1,w2,w3,w4,w5 in zip(*[s.weights for s in selected])] 
+        
+        all_weights=[s.weights for s in selected]
+        columns = list(zip(*all_weights))
+        
+        for i in range(6):
+            newstd[i]=statistics.stdev(columns[i])
+            
+        self.mean=newmean
+        self.std=newstd
+        
+    def main(self, generations):
+        #initalise population
+        self.set_population()
+        
+        for _ in generations:
+            #evaluate each game in pop
+            self.evaluate()
+            
+            #select best proportion
+            selected=self.selection()
+            
+            #update distribution
+            self.update_dist(selected)
+            
+            #create new population
+            self.set_population()
+        
+
+number_of_generations=30 
+       
+ce = CrossEntropy()  
+ce.main(number_of_generations) 
